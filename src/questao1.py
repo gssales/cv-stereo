beaae54dca5a23bc4dcb7fed8dc2bf111e02a7c1
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2021-04-29 17:24:30 -0300

    questao 1

--- src/questao1.py
+++ src/questao1.py
@@ -4,14 +4,24 @@ import argparse
 import asyncio
 import numpy as np
 import cv2
-from stereo import stereo
+from stereo import stereo, stereoAsync
+from criterios import functions as fn
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Questão 1')
-  parser.add_argument('-d', '--directory', metavar='directory', type=str, default='../datasets/cones', help='Diretório com as imagens fonte')
+  parser.add_argument('-d', '--directory', metavar='directory', type=str, default='./datasets/cones', help='Diretório com as imagens fonte')
+  parser.add_argument('-a', '--async', action='store_true', dest='a', help='Execução assíncrona')
+  parser.add_argument('-p', '--patch_size', metavar='patch_size', type=int, default=1, help='Tamanho da janela de matching')
+  parser.add_argument('-m', '--max_shift', metavar='max_shift', type=int, default=255, help='Máximo de deslocamento para procura pela disparidade')
+  parser.add_argument('-c', '--cost_fn', metavar='cost_fn', type=str, default='ssd', help='Função de custo para encontrar a disparidade')
+  parser.add_argument('-o', '--output', metavar='output', type=str, default='disp.png', help='nome do arquivo de saída')
   args = parser.parse_args()
 
   directory = args.directory
+  asyn = args.a
+  patch_size = args.patch_size
+  max_shift = args.max_shift
+  cost_fn = fn[args.cost_fn]
 
   if not os.path.isdir(directory):
     print('O diretório informado não existe')
@@ -24,12 +34,19 @@ if __name__ == '__main__':
   imgL = cv2.imread(f'{directory}/im0.png')
   imgR = cv2.imread(f'{directory}/im1.png')
 
-  disp = asyncio.run(stereo(imgL, imgR, 0))
+  disp = []
+  if asyn:
+    disp = asyncio.run(stereoAsync(imgL, imgR, cost_fn, patch_size, max_shift))
+  else:
+    disp = stereo(imgL, imgR, cost_fn, patch_size, max_shift)
   cv2.imshow("Left Image", imgL)
   cv2.imshow("Right Image", imgR)
 
-  cv2.imshow("Disparity Map", np.uint8(disp))
+  cv2.imshow(f"Disparity Map ({args.output})", np.uint8(disp))
   
   cv2.waitKey()
   cv2.destroyAllWindows()
+  
+  cv2.imwrite(args.output, np.uint8(disp))
+  print("Resultado Salvo")
 
--- src/stereo.py
+++ src/stereo.py
@@ -1,71 +1,103 @@
 import time
 import math
 import multiprocessing as mp
-  
-async def stereo(imgL, imgR, patch_size):
-  r = []
+import numpy as np
 
-  global last_time, n, elapsed
+def stereo(imgL, imgR, cost_fn, patch_size=5, max_shift=80):
+  sy, sx, _ = imgL.shape  
+  disp = np.zeros((sy, sx))
+
+  first_time = time.time()
+  last_time = time.time()
+  le = sy-2*(patch_size//2)
+  for y in range(patch_size//2, sy-patch_size//2):
+    cur_time = time.time()
+    for x in range(patch_size//2, sx-patch_size//2):
+      disp[y,x] = matchFeature(imgL, imgR, y, x, patch_size, max_shift, cost_fn)[2]
+
+    elapsed = cur_time - last_time
+    last_time = cur_time
+    seconds = int(elapsed*(le-y-(patch_size//2)))
+    minutes = seconds // 60
+    seconds = seconds - minutes*60
+    progress = ((y-(patch_size//2))*80)//sy
+    print(f'{"█"*progress}{"░"*(80-progress)}', f'{minutes} min {seconds} s  ', end='\r')
+
+  mapImg(disp, 0, max_shift, 0, 255)
+    
+  elapsed = int(last_time - first_time)
+  minutes = elapsed // 60
+  seconds = elapsed - minutes*60
+  print("█"*80, f'{minutes} min {seconds} s  ')
+  return disp
+
+async def stereoAsync(imgL, imgR, cost_fn, patch_size=5, max_shift=80):
+  global last_time, n, elapsed, sy, le
+  sy, sx, _ = imgL.shape  
+  disp = np.zeros((sy, sx))
+
+  if not cost_fn:
+    return disp
+
   elapsed = 0
+  first_time = time.time()
   last_time = time.time()
   n = 0
-  def append(result):
+  le = (sy-patch_size)*(sx-patch_size)
+  def appendResult(result):
+    y, x, d = result
+    disp[y, x] = d
+    
     cur_time = time.time()
-    r.append(result)
-    global last_time, n, elapsed
+    global last_time, n, elapsed, sy, le
     n = n+1
     elapsed = (elapsed*(n-1) + cur_time - last_time)/n
     last_time = cur_time
-    progress = (len(r)*80)//len(imgL)
-    print(f'{"█"*progress}{"░"*(80-progress)}', f'{int(elapsed*(len(imgL)-len(r)))} s  ', end='\r')
-
+    seconds = int(elapsed*(le-n))
+    minutes = seconds // 60
+    seconds = seconds - minutes*60
+    progress = (n*80)//le
+    print(f'{"█"*progress}{"░"*(80-progress)}', f'{minutes} min {seconds} s  ', end='\r')
 
   pool = mp.Pool(mp.cpu_count())
-  
-  for l in range(len(imgL)):
-    pool.apply_async(runLine, args=(imgL, imgR, l), callback=append)
-
+  for y in range(patch_size//2, sy-patch_size//2):
+    for x in range(patch_size//2, sx-patch_size//2):
+      pool.apply_async(matchFeature, args=(imgL, imgR, y, x, patch_size, max_shift, cost_fn), callback=appendResult)
   pool.close()
   pool.join()
-  print()
 
-  r.sort(key=lambda x: x[0])
-  r = [l for i, l in r]
+  mapImg(disp, 0, max_shift, 0, 255)
 
-  max_value = -10000
-  min_value = 10000
-  for l in r:
-    max_value = max(max_value, max(l))
-    for c in l:
-      min_value = min(min_value, c) if c > 0.0 else min_value
-  
-  for y in range(len(r)):
-    for x in range(len(r[y])):
-      r[y][x] = ((r[y][x] - min_value) * 255) / (max_value - min_value);
-  
-  return r
+  elapsed = int(last_time - first_time)
+  minutes = elapsed // 60
+  seconds = elapsed - minutes*60
+  print("█"*80, f'{minutes} min {seconds} s  ')
+  return disp
 
-def runLine(imgL, imgR, l):
-  line = []
-  for p in range(len(imgL[l])):
-    x = imgL[l][p]
-    d = 0
-    min_d = d
-    min_cost = 100000
-    while d <= max(p, p - len(imgL[l])/2):
-      cost = squared_diff(imgL, imgR, l, p, -d, 0)
-      last_cost = min_cost
-      min_cost = min(cost, min_cost)
-      if last_cost != min_cost:
-        min_d = d
-      d = d+1
-    line.append(abs(min_d))
-  return (l, line)
+def matchFeature(imgL, imgR, y, x, patch_size, max_shift, cost_fn):
+  sy, sx, _ = imgL.shape  
 
-def squared_diff(IL, IR, y, x, d, patch_size):
-  p = patch_size
-  s = 0
-  for i in range(3):
-    c = int(IL[y][x+d][i]) - int(IR[y][x][i])
-    s = s + c*c
-  return s
+  y0 = y-patch_size//2
+  y1 = y+patch_size//2+1
+  Lx0 = x-patch_size//2
+  Lx1 = x+patch_size//2+1
+
+  min_d = max_shift
+  min_cost = 1e10
+  for d in range(max_shift):
+    Rx0 = Lx0-d 
+    if Lx0-d < 0:
+      continue
+    Rx1 = Lx1-d #if Lx1-d < else patch_size/2+1
+
+    cost = cost_fn(imgL[y0:y1,Lx0:Lx1], imgR[y0:y1,Rx0:Rx1], 1-(d/max_shift))
+    if cost < min_cost:
+      min_cost = cost
+      min_d = d
+  return (y, x, min_d)
+
+def mapImg(img, in_min, in_max, out_min, out_max):
+  for l in range(len(img)):
+    for c in range(len(img[l])):
+      img[l][c] = ((img[l][c] - in_min) * (out_max - out_min)) // (in_max - in_min + out_min) 
+  return img
